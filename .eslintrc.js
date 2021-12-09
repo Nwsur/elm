58e6e56dbb98aeae11c7d13ba98eb491a734a4cb
Nwsur <koukutou@163.com>
2021-12-09 14:05:04 +0800

    init commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,16 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  extends: [
-    'plugin:vue/essential',
-    '@vue/standard'
-  ],
-  parserOptions: {
-    parser: 'babel-eslint'
-  },
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  }
-}
+    root: true,
+    env: {
+        node: true
+    },
+    extends: ["plugin:vue/essential", "eslint:recommended"],
+    rules: {
+        "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+        "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off", //强制使用单引号
+        quotes: ["error", "single"], //强制不使用分号结尾
+        semi: ["error", "never"] //其它规则请参考官网  https://eslint.org/docs/rules/
+    },
+    parserOptions: {
+        parser: "babel-eslint"
+    }
+};
